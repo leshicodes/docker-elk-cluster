8abe4eb60b1add64475b6e76994e7ac09f8d38ff
James Ambrose <galaxy2198@gmail.com>
2020-12-21 11:15:24 -0600

    works

--- clusterScripts/cleanUpCluster.ps1
+++ clusterScripts/cleanUpCluster.ps1
@@ -1,9 +1,15 @@
 $confirmation = Read-Host "This will remove ALL Docker containers and volumes from your system; Press 'y' to continue"
+$esPasswordFile = "elastic-passwords.txt"
 if ($confirmation -eq 'y') {
     echo "Removing ALL docker containers..."
     docker rm $(docker ps -aq) --force
     echo "Removing ALL docker volumes..."
     docker volume rm $(docker volume ls -q)
+    if(Test-Path $PSScriptRoot\..\main-cluster\$esPasswordFile)
+    {
+        echo "Removing elastic password file..."
+        rm $PSScriptRoot\..\main-cluster\$esPasswordFile
+    }
 }
 else
 {
--- clusterScripts/deployMainCluster.ps1
+++ clusterScripts/deployMainCluster.ps1
@@ -2,12 +2,14 @@ $esPasswordFile = "elastic-passwords.txt"
 $esDockerConfigFile = "elastic-docker-tls.yml"
 $secondsToSleep = 15
 
-cd ../
+cd $PSScriptRoot
+cd ../main-cluster
 docker-compose -f create-certs.yml run --rm create_certs
 docker-compose -f $esDockerConfigFile up -d
 
-echo "sleeping for $secondsToSleep seconds..."
+echo "Sleeping for $secondsToSleep seconds to initialize cluster..."
 Start-Sleep -s $secondsToSleep
+cls
 echo "Writing generated passwords to : $esPasswordFile"
 docker exec es01 /bin/bash -c "bin/elasticsearch-setup-passwords auto --batch --url https://es01:9200" >> $esPasswordFile
 
@@ -17,7 +19,11 @@ docker-compose -f $esDockerConfigFile stop
 $confirmation = Read-Host "Please navigate to `"$esDockerConfigFile`" and edit the ELASTICSEARCH_PASSWORD variable under kib01 to the user/pass combination generated in `"$esPasswordFile`"; Press 'y' to continue AFTER this is completed"
 if ($confirmation -eq 'y') {
     docker-compose -f $esDockerConfigFile up -d
+    cd $PSScriptRoot
+    cd ../
     exit 0
 }
+cd $PSScriptRoot
+cd ../
 exit 1
 
